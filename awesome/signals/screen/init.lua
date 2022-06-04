2542d8929b164aa0535c3ac16fc7f78cd939a375
Shahmir Ejaz <shaejaz5@gmail.com>
2022-06-04 17:36:02 +0500

    update screen gaps

--- awesome/signals/screen/init.lua
+++ awesome/signals/screen/init.lua
@@ -4,7 +4,20 @@ local vars = require 'config.vars'
 local widgets = require 'widgets'
 
 screen.connect_signal('request::desktop_decoration', function(s)
-    awful.tag(vars.tags, s, awful.layout.layouts[1])
+    for i, v in ipairs(vars.tags) do
+        local _selected = false
+        if i == 1 then
+            _selected = true
+        end
+        awful.tag.add(v, {
+            screen            = s,
+            layout            = awful.layout.layouts[1],
+            gap_single_client = true,
+            gap               = 10,
+            selected          = _selected
+        })
+    end
+
     s.promptbox = widgets.create_promptbox()
     s.layoutbox = widgets.create_layoutbox(s)
     s.taglist   = widgets.create_taglist(s)
